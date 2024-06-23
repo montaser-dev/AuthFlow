b3c2ebc48fd19b31152511b8c52131334da40182
montaser-dev <abdallahelmontaser786@gmail.com>
2024-06-23 16:28:44 +0100

    Setup The Project and Working with Context API

---
+++ react-frontend/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Login() {
+  const onSubmit = (ev) =>{
+    ev.preventDefault()
+  }
+  return (
+    <div className='login-signup-form animated fadeInDown'>
+      <div className="form">
+        <form onSubmit={onSubmit}>
+
+          
+        </form>
+      </div>
+      
+    </div>
+  )
+}
---
+++ react-frontend/src/router.jsx
@@ -0,0 +1,52 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Users from "./pages/Users";
+import NotFound from "./pages/NotFound";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+   {
+      path: '/',
+      element: <DefaultLayout/>,
+      children: [
+         {
+            path: '/',
+            element: <Navigate to='/users'/>
+         },
+         {    
+            path: '/dashboard',
+            element: <Dashboard />
+         }, 
+         {    
+            path: '/users',
+            element: <Users />
+         },     
+      ]
+   },
+
+   {
+      path: '/',
+      element: <GuestLayout/>,
+      children: [
+         {    
+            path: '/login',
+            element: <Login />
+         },
+        
+         {    
+            path: '/signup',
+            element: <Signup />
+         },
+      ]
+   },
+
+ {    
+    path: '*',
+    element: <NotFound />
+ },
+])
+
+export default router;
