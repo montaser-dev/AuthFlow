7936c2f38b0f4d70639afbaf208d9c2e45203a7a
montaser-dev <abdallahelmontaser786@gmail.com>
2024-07-06 20:51:59 +0100

    Fixing Logout functionalities issues

--- react-frontend/src/pages/Users.jsx
+++ react-frontend/src/pages/Users.jsx
@@ -1,17 +1,17 @@
-// import { useEffect, useState } from "react"
-// import axiosClient from '../axios-client';
+import { useEffect, useState } from "react"
+import axiosClient from '../axios-client';
 
 export default function Users() {
-  // const [users, setUsers] = useState([]);
-  // const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
+  useEffect(() => {
 
-  // }, [])
+  }, [])
 
-  // const getUsers= () => {
-  //   axiosClient.get('/users')
-  // }
+  const getUsers= () => {
+    axiosClient.get('/users')
+   }
   return (
     <div>
       Users
